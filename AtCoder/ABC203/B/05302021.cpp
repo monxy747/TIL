2643711fcdcd66ce7c2eb378b359c188eda284ea
もんくし <68159415+monxy747@users.noreply.github.com>
2021-05-31 00:00:27 +0900

    Add files via upload

--- AtCoder/ABC203/B/05302021.cpp
+++ AtCoder/ABC203/B/05302021.cpp
@@ -6,7 +6,7 @@ int main(){
   int S = 0;
   cin >> N >> K;
   int s = (K * K + K)/2;
-  
+  //cinする前にint内で計算していてWAとなったので宣言後に計算するように注意
   for(int i = 0; i < N; i++){
   	S = S + ((i + 1) * 100 * K) + s;
   }
