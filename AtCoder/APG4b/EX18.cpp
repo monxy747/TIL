8aad5e6f28bb01ce8c3e38b37be35097b79ddf18
もんくし <68159415+monxy747@users.noreply.github.com>
2021-06-02 15:21:59 +0900

    Add files via upload

---
+++ AtCoder/APG4b/EX18.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(M), B(M);
+  for (int i = 0; i < M; i++) {
+    cin >> A.at(i) >> B.at(i);
+  }
+
+  //NxNのchar型の二次元配列のすべての要素を'-'で初期化
+  vector<vector<char>> table(N, vector<char>(N, '-'));
+
+  for (int i = 0; i < M; i++) {
+    // 1~N ⇒ 0~N-1に変換
+    A.at(i)--; B.at(i)--;
+    table.at(A.at(i)).at(B.at(i)) = 'o';
+    table.at(B.at(i)).at(A.at(i)) = 'x';
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << table.at(i).at(j);
+      if ( j == N - 1) {
+        cout << endl;
+      }
+      else {
+        cout << " ";
+      }
+    }
+  }
+}
